6fe900f75071ba40005ecbe64067c73f440cf989
cofob <cofob@riseup.net>
2021-05-31 17:45:06 +0700

    Updated

--- src/com/cofob/agoj/Main.java
+++ src/com/cofob/agoj/Main.java
@@ -3,14 +3,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin {
-    public static void main (String[] string){
-        System.out.println("Test");
-    }
+    public static void main (String[] string){}
 
     @Override
     public void onEnable() {
         Logger log = getLogger();
-        log.info("Мой первый плагин!");
+        log.info("Loaded");
         getServer().getPluginManager().registerEvents(new EventsListener(), this);
     }
 }
